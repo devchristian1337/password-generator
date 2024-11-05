5015ae951279295dfa2333d8d39fabbc6eb223eb
Christian Hannachi <hannachichristian02@gmail.com>
2024-11-05 12:00:01 +0100

    Dock Fixes 2

--- src/components/ui/dock.tsx
+++ src/components/ui/dock.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import React, { PropsWithChildren, useRef } from "react";
